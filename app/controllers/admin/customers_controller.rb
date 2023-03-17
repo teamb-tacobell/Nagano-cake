e51bed4c6a7a5f5741dce9f569a54aff5ea516ab
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-03-17 09:28:51 +0000

    [Updata]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,4 +14,10 @@ class Admin::CustomersController < ApplicationController
   def update
     
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :residence, :phone_number)
+  end
 end
