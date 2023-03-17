30c29fa4ec7147e074d6141936b1dc8ad41774ab
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-03-17 16:13:58 +0000

    [add]enum機能他

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  has_one_attached :image
+  belongs_to :customer
+  has_many :order_details,  dependent: :destroy
+
+  enum cash: { credit_card:0, transfer:1 }
+  enum order_status: { waiting:0, confirm:1, making:2, preparation:3, sent:4 }
 end
