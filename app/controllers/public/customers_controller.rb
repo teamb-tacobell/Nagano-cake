c6fd27323b4342b2181c2996480647f0b84ef57f
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-03-18 09:25:49 +0000

    [Update]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,21 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customers = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customers = Customer.find(params[:id])
+  end
+  
+  def update
+    
+  end
+  
+  def quit_check
+    
+  end
+  
+  def quit
+  
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postcode, presence: true
+  validates :residence, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true
 end
