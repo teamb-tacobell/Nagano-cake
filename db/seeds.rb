331442d9bb0df626071564bf58247d9a24f69141
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-03-18 04:42:48 +0000

    seed追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-Admin.create!(
-   email: 'admin@admin',
-   password: 'tacobell'
-)
+#Admin.create!(
+#   email: 'admin@admin',
+#   password: 'tacobell'
+#)
 
+Customer.create!(
+   last_name: '鈴木',
+   first_name: '太郎',
+   last_name_kana: 'スズキ',
+   first_name_kana: 'タロウ',
+   postcode: '1500041',
+   residence: '東京都渋谷区神南1丁目19−11 パークウェースクエア2 4階',
+   phone_number: '0368694700',
+   email: 'hoge@example.com',
+   encrypted_password: '000000'
+)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
