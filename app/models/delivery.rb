775ded1c06d06e7b9e78642f7565591856bf3069
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-03-19 04:52:17 +0000

    [add]アソシエーション、pagination

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,7 +1,9 @@
 class Delivery < ApplicationRecord
-  
+
+   belongs_to :customer
+
   validates :postcode,presence:true
   validates :residence,presence:true
   validates :name,presence:true
-  
+
 end
