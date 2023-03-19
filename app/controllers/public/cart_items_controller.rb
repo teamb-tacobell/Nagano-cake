6eddb61de1b648fafae71d71e42002202a1dd646
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-03-19 07:44:56 +0000

    [add1]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,30 @@
 class Public::CartItemsController < ApplicationController
+
+  # def new
+    # @cart_item=CartItem.new
+  # end
+
+  def index
+    @cart_item=CartItem.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def all_destroy
+  end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+
 end
