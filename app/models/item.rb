e5393147e2aa0fedd0ae98d02ffcd5a8351023dc
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-03-21 09:16:15 +0000

    [Update] 検索機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
     (self.tax_excluded_price * 1.10).round
   end
 
-  def self.looks(search, word)
-    Item.where('name LIKE?', "%#{search}%")
+  def self.search(keyword)
+    where(["name like?", "%#{keyword}%"])
   end
 end
