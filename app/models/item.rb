163d41eff5c2b159ea750f3b1e1a2cd40ff2f60a
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-03-21 04:16:38 +0000

    [update]顧客注文画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
   has_many :cart_items
+  has_many :order_items
 
   validates :image, presence: true
   validates :name, presence: true
